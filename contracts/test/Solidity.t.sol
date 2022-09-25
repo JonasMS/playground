43b89bdd45fc9dd31dbdedb2e1881fe7d5902ce5
Jonas Sota <jonas@loopcrypto.xyz>
2022-09-24 22:15:32 -0600

    impl testManuallyEncodeWithSignatureMultiple()

--- contracts/test/Solidity.t.sol
+++ contracts/test/Solidity.t.sol
@@ -18,5 +18,31 @@ contract SolidityTest is Test {
         );
 
         assertEq(manualSig, actualSig, "Encodings do not match");
+        emit log_named_bytes("MANUAL SIG: ", manualSig);
+    }
+
+    function testManuallyEncodeWithSignatureMultiple() public {
+        bytes memory manualSig = bytes.concat(
+            abi.encodePacked(bytes4(keccak256("someFunc(string,uint)"))),
+            abi.encode("Some string", 9001)
+        );
+
+        bytes memory actualSig = abi.encodeWithSignature(
+            "someFunc(string,uint)",
+            "Some string",
+            9001
+        );
+
+        assertEq(manualSig, actualSig, "Encodings do not match");
+
+        emit log_named_bytes("MANUAL SIG: ", manualSig);
+        /**
+         * Bytecode:
+         * 0x672ab540 <-- Fxn selector
+         * 0000000000000000000000000000000000000000000000000000000000000040 <-- 64 bytes, 2 words
+         * 0000000000000000000000000000000000000000000000000000000000002329 <-- 9001
+         * 000000000000000000000000000000000000000000000000000000000000000b <-- ??
+         * 536f6d6520737472696e67000000000000000000000000000000000000000000 <-- "Some string"?
+         */
     }
 }

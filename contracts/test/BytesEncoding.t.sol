e6e34c17001c832a28c1b01e20a4879be20c51cf
Jonas Sota <jonas@loopcrypto.xyz>
2022-10-24 11:48:33 -0600

    rename 'Solidity.t.sol' to 'ByetEncoding.t.sol'

--- contracts/test/BytesEncoding.t.sol
+++ contracts/test/BytesEncoding.t.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.16;
 
 import "forge-std/Test.sol";
 
-contract SolidityTest is Test {
+contract BytesEncodingTest is Test {
     using stdStorage for StdStorage;
 
     function testManuallyEncodeWithSignature() public {

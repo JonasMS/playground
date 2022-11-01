fccb02b7f4be8a115e469dfbb35fb23dbcc0b0f3
De La Sota <montana.de.la.sota@proton.me>
2022-11-01 11:50:14 -0600

    set up hardhat

---
+++ hardhat.config.js
@@ -0,0 +1,27 @@
+require('@nomicfoundation/hardhat-toolbox');
+
+const {subtask} = require("hardhat/config");
+const {TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS} = require("hardhat/builtin-tasks/task-names")
+
+subtask(TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS)
+  .setAction(async (_, __, runSuper) => {
+    const paths = await runSuper();
+
+    return paths.filter(p => !p.endsWith(".t.sol"));
+  });
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+    solidity: {
+        version: '0.8.15',
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 1000000,
+            },
+        },
+    },
+    mocha: {
+        timeout: 40000,
+    },
+};
